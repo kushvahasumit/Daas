faf55118c566e39af5a766f6824f56221bbc187c
Sumit <kumarxumit6@gmail.com>
2024-03-28 22:06:12 +0530

    Basic ui structure
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 
+import './App.css'
+
 function App() {
+  const surpriseArray=[
+    "A blue panda eating pizza",
+    "A frog talking on phone",
+    "A superhero beaten by women",
+    "A baby flying on the sparrow"
+  ]
+
+  
   return (
     <div className="App">
-      <h1>Hello Daas</h1>
+      <section className="headline-section">
+        <h2> <u>Daas</u>:)</h2>
+      </section>
+      <section className="search-section">
+        <p>Start with detailed description <span className="surprise">Surprise Me</span> </p>
+        <div className="inputContainer">
+          <input type="text" placeholder="An artist paint a wall..." />
+          <button>Generate</button>
+        </div>
+      </section>
+      <section className="image-section">
+
+      </section>
     </div>
   );
 }
