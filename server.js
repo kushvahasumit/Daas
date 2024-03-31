225dcf3ac3c57d4145c34aa6de0951048dd3d8a8
Sumit <kumarxumit6@gmail.com>
2024-03-31 19:20:37 +0530

    fetching  data to frontend
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 const express = require("express");
+const cors = require('cors')
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-const route = require("../daas/routes/openAiRoute")
+
 const app = express();
 app.use(express.json());
-app.use("/openAi",route)
+app.use(cors())
 
-// enable body parser
+const { OpenAI } = require("openai");
 
-app.use(express.urlencoded({extended : false}))
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post("/genImg",async (req,res) => {
+    
+
+    try {
+        const response = await openai.images.generate({
+          model: "dall-e-2",
+          prompt: "boy flying an aeroplane",
+          n: 2,
+          quality: "standard",
+          size: "1024x1024",
+          response_format: "url",
+        });
+
+        const image_url = response.data[0].url;
+        res.status(200).json(
+          response
+        );
+        res.send(response.data.data)
+        // console.log(response.data[0].url)
+        console.log("this is backend image",image_url)
+    } catch (error) {
+         if (error.response) {
+           console.log(error.response.status);
+           console.log(error.response.data);
+         } else {
+           console.log(error.message);
+         }
+        res.status(400).json({
+            sucess:false,
+            error:"Something went wrong"
+        })
+       
+    }
+});
 
 
 app.listen(PORT, () => console.log(`Server is running on PORT : ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const surpriseArray=[
+  const [images,setImages] = useState(null)
+  const [value,setValue] = useState(null)
+
+  const surpriseMe=[
     "A blue panda eating pizza",
     "A frog talking on phone",
     "A superhero beaten by women",
     "A baby flying on the sparrow"
   ]
 
+  console.log("this is input1::",value)
+  console.log("this is images1::", images);
+
+  const getImg = async() =>{
+    try {
+      const option = {
+          method:"POST",
+          body:JSON.stringify({
+            message:"blugh"
+          }),
+          header:{
+            "Content-type":"application/json"
+          }
+      }
+      const response = await fetch('http://localhost:5000/genImg',option)
+      const data = await response.json()
+      console.log(data.data)
+      setImages(data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   
   return (
     <div className="App">
       <section className="headline-section">
-        <h2> <u>Daas</u>:)</h2>
+        <h2>
+          {" "}
+          <u>Daas</u>:)
+        </h2>
       </section>
       <section className="search-section">
-        <p>Start with detailed description <span className="surprise">Surprise Me</span> </p>
+        <p>
+          Start with detailed description{" "}
+          <span className="surprise">Surprise Me</span>{" "}
+        </p>
         <div className="inputContainer">
-          <input type="text" placeholder="An artist paint a wall..." />
-          <button>Generate</button>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            id="prompt"
+            placeholder="An artist paint a wall..."
+          />
+          <button onClick={getImg}>Generate</button>
         </div>
       </section>
+      {/* <select name="size" id="size">
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+      </select> */}
       <section className="image-section">
-
+        
+        {images?.map((image, index) => (
+          <img key={index} src={image.url} alt={`Image ${index}`} />
+        ))}
       </section>
     </div>
   );
