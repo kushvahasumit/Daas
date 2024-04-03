9518a191f1754a0ad660d8fdbc794afed50ae86a
Sumit <kumarxumit6@gmail.com>
2024-04-03 19:58:04 +0530

    remove unwanted folders
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- routes/openAiRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {generateImg }= require('../controllers/openAiController')
-const router = express.Router();
-
-
-router.post('/genImg',generateImg)
-
-module.exports = router;
